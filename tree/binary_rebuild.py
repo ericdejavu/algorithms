bf61cadadd9cf5fb7faf4e5ef4ae6f99dedd899b
吴嘉鑫 <wujiaxinxixi@qq.com>
2018-07-21 14:17:39 +0800

    Update binary_rebuild.py
    update rebuild

--- tree/binary_rebuild.py
+++ tree/binary_rebuild.py
@@ -1,7 +1,7 @@
 # root node
-pre = [1, 2, 4, 8, 16, 17, 9, 18, 19, 5, 10, 11, 3, 6, 12, 13, 7, 14, 15]
+pre_order = [1, 2, 4, 8, 16, 17, 9, 18, 19, 5, 10, 11, 3, 6, 12, 13, 7, 14, 15]
 # root left and right nodes
-mid = [16, 8, 17, 4, 18, 9, 19, 2, 10, 5, 11, 1, 12, 6, 13, 3, 14, 7, 15]
+mid_order = [16, 8, 17, 4, 18, 9, 19, 2, 10, 5, 11, 1, 12, 6, 13, 3, 14, 7, 15]
 
 def rebuild(pre,mid):
 	if not mid or not pre:
@@ -12,4 +12,4 @@ def rebuild(pre,mid):
 	root.right = rebuild(pre[sp+1:],mid[sp+1:])
 	return root
 
-root = rebuild(pre,mid)
+root = rebuild(pre_order,mid_order)
